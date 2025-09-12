0c5ae1161bfd747137244fd0f7c2b0b8c6eabf02
vicous6 <92452177+vicous6@users.noreply.github.com>
2025-09-12 11:54:34 +0200

    3 gerer la strategie de livraison (#16)
    * test ci
    
    * test ci
    
    * test ci
    
    * test ci
    
    * test ci
    
    * test ci
    
    * test ci
    
    * test ci
    
    * test ci
    
    * test ci
    
    * test ci
    
    * test ci
    
    * test ci
    
    * test ci

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -9,15 +9,27 @@ output "vm_id" {
 
 
 # Generate Ansible inventory in the root folder
-data "template_file" "ansible_inventory" {
-  template = file("${path.module}/inventory.ini.tmpl")
+data "template_file" "ansible_inventory_dev" {
+  template = file("${path.module}/inventory.ini.dev.tmpl")
   vars = {
     vm_ip          = module.vm.public_ip
     admin_username = var.admin_username
   }
 }
 
-resource "local_file" "inventory" {
-  filename = "${path.module}/inventory.ini"  # will create inventory.ini in root
-  content  = data.template_file.ansible_inventory.rendered
+data "template_file" "ansible_inventory_prod" {
+  template = file("${path.module}/inventory.ini.prod.tmpl")
+  vars = {
+    vm_ip          = module.vm.public_ip
+    admin_username = var.admin_username
+  }
+}
+
+resource "local_file" "inventory_dev" {
+  filename = "${path.module}/inventory_dev.ini"  # will create inventory.ini in root
+  content  = data.template_file.ansible_inventory_dev.rendered
+}
+resource "local_file" "inventory_prod" {
+  filename = "${path.module}/inventory_prod.ini"  # will create inventory.ini in root
+  content  = data.template_file.ansible_inventory_prod.rendered
 }
