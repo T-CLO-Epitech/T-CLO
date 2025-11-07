5e25d45aec111adbf816b8016f9551c1c653bc9d
Nicolas Tasserit <144688636+TasseritNicolas@users.noreply.github.com>
2025-11-07 17:04:28 +0100

    15 crée une task ansible pour configurer le serveur database (#57) (#58)
    * feat: Création de trois Vm (2 webserver et 1 database).
    
    * merge dev
    
    * ci test
    
    * feat: Mise en place des roles ansible
    
    * feat: Mise en place des roles ansible
    
    * feat: Mise en place des roles ansible
    
    * feat: Mise en place du déploiment de l'application web + création du .env dynamiquement
    
    * feat: Mise en place du déploiment de l'application web + création du .env dynamiquement
    
    * feat: Mise en place du déploiment de l'application web + création du .env dynamiquement
    
    * feat: Mise en place du déploiment de l'application web + création du .env dynamiquement
    
    * feat: Mise en place du déploiment de l'application web + création du .env dynamiquement
    
    * feat: Mise en place de la migration en automatique
    
    * fix conflit de merge
    
    * fix() change localisation for bdd
    
    * feat() Add a loadBalancer
    
    * merge main
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI

--- infra_iaas/resources.tf
+++ infra_iaas/resources.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.project_name}-${var.environment}-vnet"
   address_space       = ["10.10.0.0/16"]
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
@@ -16,14 +16,25 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.10.1.0/24"]
 }
-resource "azurerm_subnet_network_security_group_association" "subnet_assoc" {
-  subnet_id                 = azurerm_subnet.subnet.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
+
+resource "azurerm_virtual_network" "vnet_db" {
+  name                = "${var.project_name}-${var.environment}-vnet-db"
+  address_space       = ["10.20.0.0/16"]
+  location            = var.DB_location
+  resource_group_name = azurerm_resource_group.rg.name
 }
+
+resource "azurerm_subnet" "subnet_db" {
+  name                 = "${var.project_name}-${var.environment}-db-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet_db.name
+  address_prefixes     = ["10.20.2.0/24"]
+}
+
 
 resource "azurerm_network_security_group" "nsg" {
   name                = "${var.project_name}-${var.environment}-subnet"
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
@@ -49,4 +60,81 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "0.0.0.0/0"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "Allow-MySQL-from-AppVnet"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3306"
+    source_address_prefix      = "10.10.0.0/16"   # adresse de vnet (app)
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+
+resource "azurerm_network_security_group" "nsg_db" {
+  name                = "${var.project_name}-${var.environment}-subnet-db"
+  location            = var.DB_location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = var.allow_ssh_from_cidr
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP-8081"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8081"
+    source_address_prefix      = "0.0.0.0/0"
+    destination_address_prefix = "*"
+  }
+
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet_assoc_db" {
+  subnet_id                 = azurerm_subnet.subnet_db.id
+  network_security_group_id = azurerm_network_security_group.nsg_db.id
+}
+
+resource "azurerm_virtual_network_peering" "peer_app_to_db" {
+  name                      = "${var.project_name}-${var.environment}-peer-app-to-db"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet_db.id
+
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = false
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
+}
+
+resource "azurerm_virtual_network_peering" "peer_db_to_app" {
+  name                      = "${var.project_name}-${var.environment}-peer-db-to-app"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.vnet_db.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet.id
+
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = false
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
 }
