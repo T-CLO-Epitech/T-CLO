75d819af8a2b817ff4ae7534a926df5221f2521a
TasseritNicolas <nicolas.tasserit@epitech.eu>
2025-11-07 11:55:03 +0100

    fix CI

--- infra_iaas/variables.tf
+++ infra_iaas/variables.tf
@@ -7,3 +7,4 @@ variable "DB_location"        { type = string }
 variable "admin_username"     { type = string }
 variable "ssh_public_key"     { type = string }
 variable "allow_ssh_from_cidr"{ type = string }
+variable "actor" {type = string }
