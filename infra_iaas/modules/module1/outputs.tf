673c1ccb8aff551e629ed57c1b239fe907f51e9f
Nicolas Tasserit <144688636+TasseritNicolas@users.noreply.github.com>
2025-11-07 17:00:56 +0100

    15 crée une task ansible pour configurer le serveur database (#57)
    * feat: Création de trois Vm (2 webserver et 1 database).
    
    * merge dev
    
    * ci test
    
    * feat: Mise en place des roles ansible
    
    * feat: Mise en place des roles ansible
    
    * feat: Mise en place des roles ansible
    
    * feat: Mise en place du déploiment de l'application web + création du .env dynamiquement
    
    * feat: Mise en place du déploiment de l'application web + création du .env dynamiquement
    
    * feat: Mise en place du déploiment de l'application web + création du .env dynamiquement
    
    * feat: Mise en place du déploiment de l'application web + création du .env dynamiquement
    
    * feat: Mise en place du déploiment de l'application web + création du .env dynamiquement
    
    * feat: Mise en place de la migration en automatique
    
    * fix conflit de merge
    
    * fix() change localisation for bdd
    
    * feat() Add a loadBalancer
    
    * merge main
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI
    
    * fix CI

--- infra_iaas/modules/module1/outputs.tf
+++ infra_iaas/modules/module1/outputs.tf
@@ -1,9 +1,16 @@
-#Affiche les données de l'instance créée
-
 output "public_ip" {
-  value = azurerm_public_ip.pip.ip_address
+  description = "Adresses IP publiques par rôle (compatibilité)"
+  value = merge(
+    { for role, pip in azurerm_public_ip.pip : role => pip.ip_address },
+    { for role, pip in azurerm_public_ip.pip_database : role => pip.ip_address }
+  )
 }
 
+
 output "vm_id" {
-  value = azurerm_linux_virtual_machine.vm.id
+  description = "IDs des VMs par rôle (compatibilité)"
+  value = merge(
+    {for role, vm in azurerm_linux_virtual_machine.vm : role => vm.id},
+    {for role, vm in azurerm_linux_virtual_machine.vm_database : role => vm.id}
+  )
 }
--- infra_iaas/variables.tf
+++ infra_iaas/variables.tf
@@ -3,6 +3,7 @@ variable "project_name"       { type = string }
 variable "environment"        { type = string }
 variable "location"           { type = string }
 variable "vm_size"            { type = string }
+variable "DB_location"        { type = string }
 variable "admin_username"     { type = string }
 variable "ssh_public_key"     { type = string }
 variable "allow_ssh_from_cidr"{ type = string }
